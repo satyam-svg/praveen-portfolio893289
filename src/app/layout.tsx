73bb29bc958c3fd5b425bd7092c4d5fc7321eb84
Praveen Maurya <85008525+satyam-svg@users.noreply.github.com>
2024-06-24 15:29:13 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Praveen Maurya",
   description: "This is Praveen Maurya portfolio made in 3d using three js and react three fibre",
+  icons:"https://iampraveen.hi.link/_assets/site-data/ops_6hOSF6QUzR0lDZS8vR/images/2097ca8a117cce71f84e4ff853ef61097aedac06f1d68604d93a1ba407f80e31.png"
 };
 
 export default function RootLayout({
